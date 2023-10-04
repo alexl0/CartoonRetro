bcf31ae7631d0f1639664bbc84f6fe06c4375b18
IDENT\UO258774 <alexleon8898@gmail.com>
2023-10-04 16:11:04 +0200

    Modified html project to also scroll vertically when needed. Now I have to modify the java code to modify the your-text-file.txt file. Then, the HTML project handles the rest.

--- obs/planificationHTML/script.js
+++ obs/planificationHTML/script.js
@@ -1,5 +1,12 @@
 document.addEventListener("DOMContentLoaded", function () {
-    const linesList = document.getElementById("lines-list");
+    const list = document.getElementById("lines-list");
+    const viewport = document.querySelector('.viewport');
+    let listItemHeight;
+    let visibleElements = 40; // Number of visible elements
+    let scrollOffset = 0;
+    let isScrollingToEnd = true;
+    const scrollDuration = 8000; // 8 seconds
+    const pauseDuration = 60000; // 60 seconds
 
     fetch("your-text-file.txt")
         .then(response => response.text())
@@ -11,7 +18,7 @@ document.addEventListener("DOMContentLoaded", function () {
             lines.forEach(lineText => {
                 const li = document.createElement("li");
                 li.textContent = lineText;
-                linesList.appendChild(li);
+                list.appendChild(li);
 
                 // Check if the line needs scrolling logic
                 if (li.scrollWidth > li.clientWidth) {
@@ -19,12 +26,82 @@ document.addEventListener("DOMContentLoaded", function () {
                 }
             });
 
+            // Calculate the number of empty items to add to make the count multiple of visibleElements
+            const remainder = lines.length % visibleElements;
+            const emptyItemCount = remainder === 0 ? 0 : visibleElements - remainder;
+            for (let i = 0; i < emptyItemCount; i++) {
+                const emptyListItem = document.createElement('li');
+                list.appendChild(emptyListItem);
+            }
+
             // Trigger reflow to apply the animation
-            linesList.style.display = "none";
-            linesList.offsetHeight;
-            linesList.style.display = "block";
+            list.style.display = "none";
+            list.offsetHeight;
+            list.style.display = "block";
+
+            // Calculate the listItemHeight after the list is generated
+            listItemHeight = list.firstElementChild.getBoundingClientRect().height;
+
+            setInterval(scrollList, scrollDuration + pauseDuration);
+
         })
         .catch(error => {
             console.error("Error loading text file:", error);
         });
+
+
+
+    function scrollList() {
+        const scrollAmount = listItemHeight * visibleElements;
+        const maxScroll = list.scrollHeight - viewport.clientHeight;
+    
+        if (isScrollingToEnd) {
+            scrollOffset += scrollAmount;
+    
+            if (scrollOffset >= maxScroll) {
+            list.style.transition = `transform ${scrollDuration / 1000}s linear`;
+            list.style.transform = `translateY(-${maxScroll}px)`;
+            setTimeout(() => {
+                list.style.transition = 'none';
+                isScrollingToEnd = false;
+                scrollOffset = maxScroll;
+                list.style.transform = `translateY(-${scrollOffset}px)`;
+                setTimeout(() => {
+                list.style.transition = `transform ${scrollDuration / 1000}s linear`;
+                scrollOffset -= scrollAmount;
+                if (scrollOffset < maxScroll) {
+                    scrollOffset = maxScroll;
+                }
+                list.style.transform = `translateY(-${scrollOffset}px)`;
+                }, 10);
+            }, scrollDuration + pauseDuration);
+            } else {
+            list.style.transition = `transform ${scrollDuration / 1000}s linear`;
+            list.style.transform = `translateY(-${scrollOffset}px)`;
+            }
+        } else {
+            scrollOffset -= scrollAmount;
+    
+            if (scrollOffset <= 0) {
+            list.style.transition = 'none';
+            isScrollingToEnd = true;
+            scrollOffset = 0;
+            list.style.transform = `translateY(-${scrollOffset}px)`;
+            setTimeout(() => {
+                list.style.transition = `transform ${scrollDuration / 1000}s linear`;
+                scrollOffset -= scrollAmount;
+                if (scrollOffset < 0) {
+                scrollOffset = 0;
+                }
+                list.style.transform = `translateY(-${scrollOffset}px)`;
+            }, 10);
+            } else {
+            list.style.transition = `transform ${scrollDuration / 1000}s linear`;
+            list.style.transform = `translateY(-${scrollOffset}px)`;
+            }
+        }
+    }
+
 });
+
+
